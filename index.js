68e2de9a1ea99b3f110ab3c5ce8114acf3fac866
Viktor Larin <Larinv1983@mail.ru>
2022-08-17 16:43:33 +0300

    валидация

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import {registerValidation} from './validations/auth.js';
+import {validationResult} from 'express-validator';
+import UserModel from './models/User.js';
+
 
 mongoose.connect('mongodb+srv://vandal:ovjfmxa6@cluster0.sgvqocw.mongodb.net/?retryWrites=true&w=majority')
-.then(() => console.log('Database OK'))
-.catch((err) => console.log('Database ERROR', err));
+.then(() => console.log('Database OK:'))
+.catch((err) => console.log('Database ERROR:', err));
 
 const app = express();
 
@@ -14,19 +18,13 @@ app.get('', (req, res) => {
 	res.send('Всем здрасте!');
 });
 
-app.post('/login', (req, res) => {
-	console.log(req.body);
-
-	const token = jwt.sign({
-		email: req.body.email,
-		fullName: 'Виктор Ларин',
-	},
-	'randompassword',
-	);
-
+app.post('/login', registerValidation, (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json(errors.array());
+	}
 	res.json({
 		success: true,
-		token,
 	});
 });
 
